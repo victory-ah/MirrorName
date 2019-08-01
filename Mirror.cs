a00fe1a6a0866b4ed4d74c42ff3ed286b9c6c824
victory-ah <52970645+victory-ah@users.noreply.github.com>
2019-08-01 09:49:31 -0700

    Add files via upload

---
+++ Mirror.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace WhiteBoardAct
+{
+    class Mirror
+    {
+
+        public static string MirrorName(string firstName)
+        {
+            char[] firstNameArray = firstName.ToCharArray();
+            Array.Reverse(firstNameArray);
+
+            string result = "";
+
+            foreach (char item in firstNameArray)
+            {
+                result += item;
+            }
+
+            return result;
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's your first name?");
+            string firstName = Console.ReadLine();
+
+            string result = MirrorName(firstName);
+
+            Console.WriteLine("Results: " + result);
+
+            Console.ReadLine();
+
+            {
+
+            }
+        }
+
+       
+            
+
+    }
+}
